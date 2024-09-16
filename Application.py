6845505d6571959b03aac0ccfd08cd1c40b07019
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-16 10:33:39 -0400

    Obtain the user input and use it to add the two numbers and print the result

--- Application.py
+++ Application.py
@@ -1,7 +1,12 @@
 """The Application module is responsible for the interaction with the user"""
+import Calculator
 
 #ask the user for two numbers
+num1Input = input("Please enter the first number")
+num2Input = input ("Please enter the second number")
 
 #use the calculator module to add them
+sum = Calculator.add(num1Input, num2Input)
 
 #let the user know what the sum of the two numbers is
+print(f"{num1Input} + {num2Input} = {sum}")
