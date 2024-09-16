f74edbc3273a8a2ec55c4310079b25495fc1ae84
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-16 10:04:56 -0400

    Define the add function.

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,6 @@
 """The Calculator module is responsible for all the business logic: artihmetic operations"""
 
 #define a function that adds two numbers provided as arguments and returns their sum
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
