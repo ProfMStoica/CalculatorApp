3f253cf5e7f415a3fd08556e6a9aa2738863241c
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-16 09:55:24 -0400

    Established a development plan

--- Application.py
+++ Application.py
@@ -1,1 +1,7 @@
 """The Application module is responsible for the interaction with the user"""
+
+#ask the user for two numbers
+
+#use the calculator module to add them
+
+#let the user know what the sum of the two numbers is
--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,3 @@
 """The Calculator module is responsible for all the business logic: artihmetic operations"""
+
+#define a function that adds two numbers provided as arguments and returns their sum
