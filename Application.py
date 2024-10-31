26e6ba57bf6c7a6062a2d201a113fb58d5865bc2
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-31 09:56:10 -0400

    Convert the string user input into numbers so they can be used in the calculator.

--- Application.py
+++ Application.py
@@ -3,10 +3,12 @@ import Calculator
 
 #ask the user for two numbers
 num1Input = input("Please enter the first number: ")
+firstNum = float(num1Input)
 num2Input = input ("Please enter the second number: ")
+secondNum = float(num2Input)
 
 #use the calculator module to add them
-result = Calculator.add(num1Input, num2Input)
+result = Calculator.add(firstNum, secondNum)
 
 #let the user know what the sum of the two numbers is
-print(f"{num1Input} + {num2Input} = {result}")
+print(f"{firstNum} + {secondNum} = {result}")
