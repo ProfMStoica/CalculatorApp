3076f4b8bdf96cba56bfe5e5edfd51c29934d2ca
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-31 10:29:40 -0400

    Define the method structure of the MathApp class

--- mathapp.py
+++ mathapp.py
@@ -4,3 +4,30 @@ from calculator import  Calculator
 class MathApp:
     def __init__(self):
         self._calc = Calculator()
+
+    def run(self):
+        pass
+
+    def showMenu(self):
+        pass
+
+    def runAddition(self):
+        pass
+
+    def runSubtraction(self):
+        pass
+
+    def runMultiplication(self):
+        pass
+
+    def runDivision(self):
+        pass
+
+    def runIntDivision(self):
+        pass
+
+    def runDivRemainder(self):
+        pass
+
+    def runQuadratic(self):
+        pass
